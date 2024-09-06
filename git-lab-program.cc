4ecddda24fe43831cd2bfb04f7ea255df0d6888a
Matthew <mi465623@ohio.edu>
2024-09-06 15:25:32 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Matthew Isham>
+ *        Date: <09/06/24>
+ * Description: lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
